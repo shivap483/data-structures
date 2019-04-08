f46db16b3c6a1aeac4cd3d2663f50ba33979ab81
shiva <shiva@shiva-Vostro-3478>
2019-04-08 09:50:15 +0530

    changes..

--- G4G_python/g4g_array_rotation/check_pair_with_sum_in_sorted_rotated_array.py
+++ G4G_python/g4g_array_rotation/check_pair_with_sum_in_sorted_rotated_array.py
@@ -3,37 +3,42 @@ Created on 27-Feb-2019
 
 @author: shiva
 '''
-def findPivot(array):
-    if len(array)<1:
-        return -1
-    for i in range(len(array)-1):
-        if(array[i]>array[i+1]):
-            return i
-    return len(array)
 
-def hasPair(array,sum):
-    n=len(array)
-    l=findPivot(array)
-    r=(l+1)%n
-    count=0
-    while(l!=r):
-        s=array[l]+array[r]
-        if(s==sum):
-            count=count+1            
-            if r==(l+n-1)%n:
+
+def findPivot(array):
+    n = len(array)
+    if n < 1:
+        return -1
+    for i in range(n - 1):
+        if(array[i] > array[i + 1]):
+            return i
+    return n - 1
+
+
+def hasPair(array, sum):
+    n = len(array)
+    l = findPivot(array)
+    r = (l + 1) % n
+    count = 0
+    while(l != r):
+        s = array[l] + array[r]
+        if(s == sum):
+            count = count + 1            
+            if r == (l + n - 1) % n:
                 break
-            l=(n+l-1)%n
-            r=(r+1)%n
-        elif(s>sum):
-            l=(n+l-1)%n
+            l = (n + l - 1) % n
+            r = (r + 1) % n
+        elif(s > sum):
+            l = (n + l - 1) % n
         else:
-            r=(r+1)%n
+            r = (r + 1) % n
     return count
+
         
-a=[10,18,45,-8,0,1,2,3,4,5,6,7]
-sum=7
-res=hasPair(a, sum)
-if res>=1:
+a = [ 10, 15, 18, 45, -8, 0, 1, 2, 3, 4, 5, 6, 7]
+sum = 7
+res = hasPair(a, sum)
+if res >= 1:
     print(res)
 else:
     print('no pair')
--- G4G_python/g4g_array_rotation/maximum_value_of_sum_iarri_with_only_rotations.py
+++ G4G_python/g4g_array_rotation/maximum_value_of_sum_iarri_with_only_rotations.py
@@ -1,22 +1,23 @@
 def arraySum(arr):
-	aSum=0
-	for i in arr:
-		aSum=aSum+i;
-	return aSum;
+	aSum = 0
+	curVal = 0;
+	for i in range(len(arr)):
+		curVal = curVal + i * arr[i];
+		aSum = aSum + arr[i];
+	return aSum, curVal;
+
 
 def findMaxSum(arr):
-	arrSum=arraySum(arr);
-	n=len(arr)
-	curVal=0
-	for i in range(n):
-		curVal=curVal+i*arr[i];
+	arrSum, curVal = arraySum(arr);
+	n = len(arr)
 	
-	maxVal=curVal
-	for i in range(1,n):
-		curVal=arrSum-n*arr[n-i]+curVal
-		if(curVal>maxVal):
-			maxVal=curVal
+	maxVal = curVal
+	for i in range(1, n):
+		curVal = arrSum - n * arr[n - i] + curVal
+		if(curVal > maxVal):
+			maxVal = curVal
 	return maxVal
 
-arr=[1,20,2,10]
+
+arr = [1, 20, 2, 10]
 print(findMaxSum(arr))
