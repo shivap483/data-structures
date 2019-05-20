b711406ba7ccef6348278b1f96a6b845733e20e2
shiva <shivap483@gmail.com>
2019-05-20 09:33:01 +0530

    linkedList- still not working.

--- G4G_python/g4g_linkedList/Node.py
+++ G4G_python/g4g_linkedList/Node.py
@@ -7,7 +7,11 @@ Created on 11-Apr-2019
 
 class Node:
 
-    def __init__(self, data):
+    def __init__(self, data=None):
         self.data = data
         self.next = None
         self.prev = None
+
+# class LinkedList:
+#     def __init__(self):
+#         self.head=Node()
