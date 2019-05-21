ac0cc2c3f9d152153f4ad70a66b449bec3fe8f93
shiva <shivap483@gmail.com>
2019-05-21 08:56:19 +0530

    kfdjnvkdfnv

--- G4G_python/g4g_linkedList/linkedList.py
+++ G4G_python/g4g_linkedList/linkedList.py
@@ -9,16 +9,31 @@ class LinkedList:
     def append(self,data):
         new_node=Node(data)
         current=self.head
-        while current.next!=None:
+
+        while current!=None:
             current=current.next
-        current.next=new_node
-        print(current.data)
+        current=new_node
+        #print(current.data)
 
     def printList(self):
         node=self.head
-        while node.next!=None:
-            node=node.next
+        while node!=None:
             print(node.data)
+            node=node.next
+
+
+    def deleteNodeByValue(self, data):
+        node=self.head
+        if(node.data==data):
+            node=node.next
+            return
+        while node.next!=None:
+            temp=node.next
+            if(temp.data==data):
+                node.next=temp.next
+                break
+            node=node.next
+
 
 linkedList=LinkedList()
 linkedList.append(9)
@@ -28,3 +43,5 @@ linkedList.append(0)
 linkedList.append(5)
 linkedList.append(34)
 linkedList.printList()
+#linkedList.deleteNodeByValue(5)
+#linkedList.printList()
