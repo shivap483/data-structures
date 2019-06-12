0e21bd95b94761926bf31801a51fafe823df692c
shiva <shivap483@gmail.com>
2019-06-12 10:20:40 +0530

    optimised

--- G4G_python/g4g_linkedList/detectLoop.py
+++ G4G_python/g4g_linkedList/detectLoop.py
@@ -1,4 +1,4 @@
-from linkedList import LinkedList as ll
+from g4g_linkedList.linkedList import LinkedList as ll
 
 ll.append(2)
 ll.append(1)
--- G4G_python/g4g_linkedList/linkedList.py
+++ G4G_python/g4g_linkedList/linkedList.py
@@ -5,10 +5,13 @@ class LinkedList:
     """docstring for LinkedList."""
 
     def __init__(self):
-        self.head = Node()
+        self.head = None
 
     def append(self, data):
         new_node = Node(data)
+        if self.head == None:
+            self.head = new_node
+            return
         current = self.head
         while current.next != None:
             current = current.next
@@ -16,38 +19,48 @@ class LinkedList:
 
     def printList(self):
         node = self.head
-        while node.next != None:
-            node = node.next
+        while node != None:
             print(node.data)
-
-    def deleteNodeByValue(self, data):
-        node = self.head
-        while node.next != None:
-            temp = node.next
-            if(temp.data == data):
-                node.next = temp.next
-                break
             node = node.next
+
+    def deleteNodeByValue(self, key):
+        temp = self.head
+        if(self.head == None):
+            return 
+        if(temp.data == key):
+            self.head = temp.next
+            temp = None
+            return 
+        while temp != None:
+            if(temp.data == key):
+                break
+            prev = temp
+            temp = temp.next
+        if(temp != None):
+            prev.next = temp.next
+            temp = None
 
     def push(self, data):
         new_node = Node(data)
-        head = self.head
-        new_node.next = head.next
-        head.next = new_node
+        top = self.head
+        new_node.next = top
+        self.head = new_node
 
     def pop(self):
         temp = self.head
+        if(self.head == None):
+            return None
         if(temp.next != None):
             temp = temp.next
-            self.head.next = temp.next
+            self.head = temp
         return temp
 
     def insert(self, n, data):
         new_node = Node(data)
         temp = self.head
         if(n == 1):
-            new_node.next = self.head.next
-            self.head.next = new_node
+            new_node = self.head
+            self.head = new_node
             return
         for i in range (n - 1):
             temp = temp.next
@@ -55,12 +68,12 @@ class LinkedList:
         temp.next = new_node
 
     def lengthByiteration(self):
-        temp = self.head.next
+        temp = self.head
         i = 0
-        while(temp):
+        while(temp.next != None):
             temp = temp.next
             i += 1
-        return i
+        return i + 1
 
     def lengthByRecursion(self, node):
         if(not node):
@@ -68,17 +81,17 @@ class LinkedList:
         return 1 + self.lengthByRecursion(node.next)
 
     def getLength(self):
-        return self.lengthByRecursion(self.head.next)
+        return self.lengthByRecursion(self.head)
 
     def createLoop(self):
-        self.head.next.next.next.next.next = self.head.next
+        self.head.next.next.next.next = self.head.next
         return
 
     def detectLoop(self):
         flag = 0
-        slow = self.head.next
-        fast = self.head.next
-        while(slow and fast and fast.next and fast.next.next):
+        slow = self.head
+        fast = self.head
+        while(slow.data and fast.data and fast.next.data and fast.next.next.data):
             if(slow == fast and flag != 0):
                 print('found')
                 count = 1
@@ -86,14 +99,15 @@ class LinkedList:
                 while(slow != fast):
                     slow = slow.next
                     count += 1
-                return count
+                print(count) 
+                return
 
             slow = slow.next
             fast = fast.next.next
             flag = 1
         print('not found')
 
-
+'''
 linkedList = LinkedList()
 linkedList.pop()
 linkedList.append(9)
@@ -117,8 +131,9 @@ print ('')
 linkedList.insert(2, 55)
 linkedList.printList()
 print ('')
-print(linkedList.lengthByiteration())
-print(linkedList.getLength())
+print('length by iteration: ', linkedList.lengthByiteration())
+print('length by recurssion: ', linkedList.getLength())
 
 linkedList.createLoop()
-print(linkedList.detectLoop())
+linkedList.detectLoop()
+'''
